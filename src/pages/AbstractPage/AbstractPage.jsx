89a532f8d54861580e6bb439e08051ce84945da0
Diego Garcia <digarcia@icsecurity.com>
2024-07-05 00:18:45 -0600

    Menu lateral migrado

--- src/pages/AbstractPage/AbstractPage.jsx
+++ src/pages/AbstractPage/AbstractPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import MenuContainer from '../../components/LateralMenu/MenuContainer'
-import MenuItem from '../../components/LateralMenu/MenuItem'
-import SubMenu from '../../components/LateralMenu/SubMenu'
+import LateralMenu from '@encit/web-ui-components/LateralMenu'
 import styles from '../Spa.module.css'
 import PropTypes from 'prop-types';
 
@@ -10,23 +8,7 @@ const AbstractPage = ({ menuTree }) => {
 
     return (<div className={styles.mainContainer}>
         <div className={styles.lateralMenu}>
-            <MenuContainer>
-                {menuTree.map((element, index) =>
-                    <React.Fragment key={element.id}>
-                        {element.itemResource ?
-                            <MenuItem label={element.itemLabel} to={element.itemResource} key={element.id}/>
-                            :
-                            <MenuItem label={element.itemLabel}  key={element.id}>
-                                <SubMenu>
-                                    {element.itemContent.map((subElement, subIndex) =>
-                                        <MenuItem label={subElement.itemLabel} to={subElement.itemResource} key={`${element.id}_${subElement.id}`}/>
-                                    )}
-                                </SubMenu>
-                            </MenuItem>
-                        }
-                    </React.Fragment>
-                )}
-            </MenuContainer>
+            <LateralMenu menuOptions={menuTree}></LateralMenu>
         </div>
         <div className={styles.spaContent}>
             <div className={styles.spaInnerContent}>
